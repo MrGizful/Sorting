8ecfe0437c86c93ebac9bddfb3996ba52d94032f
Игорь Воротников <igor.vorotnikov@yandex.ru>
2021-03-04 10:56:39 +0300

    Add O-notations to bubble, selection and insertion sorts.

--- Sorting/BubbleSort.h
+++ Sorting/BubbleSort.h
@@ -3,9 +3,10 @@
 class BubbleSort
 {
 public:
-
+	//Best, average and worst cases - O(n^2)
 	std::vector<int> sort(std::vector<int> array);
 
+	//Best case - O(n), average and worst cases - O(n^2)
 	std::vector<int> improvedSort(std::vector<int> array);
 };
 
--- Sorting/InsertionSort.h
+++ Sorting/InsertionSort.h
@@ -4,6 +4,7 @@
 class InsertionSort
 {
 public:
+	//Best, average and worst cases - O(n^2)
 	std::vector<int> sort(std::vector<int> array);
 };
 
